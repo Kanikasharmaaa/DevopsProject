797e606abb90b535a8b4b22537c13e6afb1d995b
Kanikasharmaaa <101467636+Kanikasharmaaa@users.noreply.github.com>
2022-05-27 12:05:22 -0400

    Update Terraformeks_cluster.tf

--- Terraformeks_cluster.tf
+++ Terraformeks_cluster.tf
@@ -43,16 +43,11 @@ module "vpc" {
   azs  = data.aws_availability_zones.available_azs.names
 
   private_subnets = [
-    # this loop will create a one-line list as ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20", ...]
-    # with a length depending on how many Zones are available
     for zone_id in data.aws_availability_zones.available_azs.zone_ids :
     cidrsubnet(var.main_network_block, var.subnet_prefix_extension, tonumber(substr(zone_id, length(zone_id) - 1, 1)) - 1)
   ]
 
   public_subnets = [
-    # this loop will create a one-line list as ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", ...]
-    # with a length depending on how many Zones are available
-    # there is a zone Offset variable, to make sure no collisions are present with private subnet blocks
     for zone_id in data.aws_availability_zones.available_azs.zone_ids :
     cidrsubnet(var.main_network_block, var.subnet_prefix_extension, tonumber(substr(zone_id, length(zone_id) - 1, 1)) + var.zone_offset - 1)
   ]
